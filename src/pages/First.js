4a1b98436bd749338247798a1008b878064ebe62
Sci Steps <scistepsug@gmail.com>
2025-08-08 00:46:46 -0800

    them

--- src/pages/First.js
+++ src/pages/First.js
@@ -417,7 +417,7 @@ const videoRefs = {
          className="text-container"> 
         <p className="left-aligned">
   Welcome to <span className="highlight">Rejuv</span> —  We are a community of animators led and founded by  
-   <span className="highlight bold"> Sam Nkurunungi.</span> "We work on different projects ranging from <span className="highlight"> animated short stories, animated explainers, music & lyric videos</span> and other passion projects that come along the way."
+   <span className="highlight bold"> Sam Nkurunungi.</span> "We work on different projects ranging from <span className="highlight"> animated short stories, animated explainers, music & lyric videos</span> and other passion projects, scroll down to view them.
   
 </p>
 
