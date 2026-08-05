2d1cc21ffe6c0272a7049b6bd7387c5fef4e2897
scisteps <scistepsug@gmail.com>
2026-08-04 18:26:56 -0800

    removed excess

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import First from './pages/First';
-import AddVideo from './entries/AddVideo';
-import Theteam from './pages/Theteam';
+
 import AdminDashboard from './mapages/AdminDashboard';
 import UserDashboard from './mapages/UserDashboard';
-import AddContributor from './entries/AddContributor';
-import MightyMorphin from './tokens/MightyMorphin';
-import Agnes from './tokens/Agnes';
-import Brand from './pages/Brand';
+
 import Members from './pages/Members';
-import Tabitha from './tokens/Tabitha';
 import { Analytics } from "@vercel/analytics/react"
-import Third from './pages/Third';
-import Tsam from './Tsam/Tsam';
-import Profile from './pages/Profile';
-import PopCulture from './pages/PopCulture';
+
 
 function App() {
   return (
@@ -30,17 +21,9 @@ function App() {
           <Route path="/admin" element={<AdminDashboard />} />
           
          
-          <Route path="/first" element={<First />} />
-          <Route path="/addvideo" element={<AddVideo />} />
-          <Route path="/team" element={<Theteam />} />
-          <Route path="/addcontributor" element={<AddContributor />} />
-          <Route path="/morphintime" element={<MightyMorphin />} />
           <Route path="/profiles" element={<Members />} />
-          <Route path="/tabi" element={<Tabitha />} />
-          <Route path="/stillgraphics" element={<Third />} />
-          <Route path="/tsam" element={<Tsam />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/pc" element={<PopCulture />} />
+    
+     
         
           {/* Catch-all route - 404 Not Found */}
           <Route path="*" element={<div className="not-found">
